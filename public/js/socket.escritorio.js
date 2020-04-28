8e4fdb30fcb85e052672bcf60108b541dec13838
mjmendez <majomendez88mena@gmail.com>
2020-04-28 10:06:30 -0600

    Primer commit produccion

---
+++ public/js/socket.escritorio.js
@@ -0,0 +1,30 @@
+var socket = io();
+
+
+let label = $('small');
+
+var searchParams = new URLSearchParams(window.location.search);
+if (!searchParams.has('escritorio')) {
+    window.location = 'index.html';
+    throw new Error('El escritorio es necesario');
+}
+
+
+var escritorio = searchParams.get('escritorio');
+console.log(escritorio);
+
+
+$('h1').text('Escritorio: ' + escritorio);
+
+$('button').on('click', function() {
+    socket.emit('atenderTicket', { escritorio: escritorio }, function(resp) {
+
+        if (resp === 'No hay tickets en cola') {
+            label.text(resp);
+            return;
+        }
+        label.text('Ticket ' + resp.numero);
+
+    });
+
+});
---
+++ public/js/socket.nuevo-ticket.js
@@ -0,0 +1,28 @@
+//Comando para establecer la comunicacion
+
+
+var socket = io();
+var label = $('#lblNuevoTicket');
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor')
+
+});
+
+socket.on('disconnect', function() {
+    console.log('Desconectado al servidor')
+});
+
+//on 'estadoActial' recibe actual, y lo coloco en el label
+
+socket.on('estadoActual', function(resp) {
+    label.text(resp.actual);
+});
+
+
+$('button').on('click', function() {
+    socket.emit('siguienteTicket', null, function(siguienteTicket) {
+        label.text(siguienteTicket);
+
+    });
+});
